bd0e11649ec92c866af8f3338d21a9a031e5bd95
Patience <108203559+PatienceStPim@users.noreply.github.com>
2025-04-03 03:33:15 +0100

    Add files via upload

--- data/alignment.sh
+++ data/alignment.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
+#Make directories to hold outputs for different MSA methods
 mkdir alignment alignment/clustalo alignment/mafft alignment/einsi
 #Create alignment using clustal omega on default settings.
-clustalo -i Norovirus_PP.fasta -o alignment/clustalo/Norovirus_clustalo_.fst
+clustalo -i Norovirus_PP.fasta -o alignment/clustalo/Norovirus_clustalo.fst
 #Alignment by MAFFT in sequential mode
 mafft --maxiterate 1000 Norovirus_PP.fasta > alignment/mafft/Norovirus_mafft.fst
 #Alignment by MAFFT in E-INS-i mode
 mafft --maxiterate 1000 --genafpair Norovirus_PP.fasta > alignment/einsi/Norovirus_einsi.fst
-#Survey each MSA using AliStat in amino acid mode (1)
+
+#Survey each MSA using AliStat in amino acid mode (6) and store the output in the relevant directory. We are interested in Cc histograms and summary (-t 2)
 #Clustalo
-alistat alignment/clustalo/Norovirus_clustalo_.fst 6 -i -o alignment/clustalo/clustalo
-#MAFFT in sequential mode 
-alistat alignment/mafft/Norovirus_mafft.fst 6 -i -o alignment/mafft/mafft
-#MAFFT in E-INS-i mode
-alistat alignment/einsi/Norovirus_einsi.fst 6 -i -o alignment/einsi/einsi
+for i in clustalo mafft einsi
+do
+    mkdir alignment/$i/alistat 
+    alistat alignment/$i/Norovirus_$i.fst 6 -t 2 -o $i
+    R CMD BATCH $i.Histogram_Cc.R
+    mv $i* alignment/$i/alistat
+done
+
+#Masking using Alistat by outputting the alignment where sites have a Cc > 0.3 based off Cc distribution. Masked fasta file and summary.txt stored in relevant directory
+for i in clustalo mafft einsi
+do 
+    mkdir alignment/$i/alistat_masked
+    alistat alignment/$i/Norovirus_$i.fst 6 -m 0.3 -o $i
+    #generated summary file is of the original alignment
+    rm $i.Summary.txt
+    #rerun alistat on the masked alignment in brief mode
+    alistat $i.Mask.fst 6 -o $i
+    mv $i* alignment/$i/alistat_masked
+done
+
+#Run alignments through SatuRation and SatuRationHeatMapper
+mkdir alignment/mafft/saturation
+saturation alignment/mafft/Norovirus_mafft.fst v f 30
+mv 
